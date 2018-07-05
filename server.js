94337f6ebd70e198ab31b8b11c1b8c83893659e8
Philippe Recto <precto1285@gmal.com>
2018-07-05 14:06:00 -0700

    added server

--- server.js
+++ server.js
@@ -1,15 +1,18 @@
 const express = require('express');
 const cors = require('cors');
+const mysql = require('mysql');
 
 const app = express();
 
-const SELECT_ALL_PRODUCTS_QUERY = 'SELECT * FROM products';
+const selectAll = 'SELECT * FROM products'
+
+const SELECT_ALL_USERS_QUERY = 'SELECT * FROM user_Info';
 
 const connection = mysql.createConnection({
 host: 'localhost',
 user: 'root',
-passwordL 'theo',
-database: 'react_sql'
+password: 'root',
+database: 'reactsql'
 });
 
 connection.connect(err => {
@@ -21,8 +24,20 @@ connection.connect(err => {
 app.use(cors());
 
 app.get('/', (req, res)=> {
-    res.send('go to/products to see products');
-}); 
+    res.send('go to /products to see products')
+    }); 
+
+    app.get('/userInfo', (req, res) => {
+        connection.query(SELECT_ALL_USERS_QUERY, (err, results) => {
+            if(err) {
+                return res.send(err)
+            }
+            else {
+                return res.json({
+                    data: results
+                })
+            }
+    });
 
 app.listen(3000, () => {
     console.log('Listening to server on port 3000');
